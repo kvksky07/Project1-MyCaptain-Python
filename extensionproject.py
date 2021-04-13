8beacf4671960e941c11fc60fda523984c284822
Kshitij Kakde <82435148+kvksky07@users.noreply.github.com>
2021-04-13 12:15:59 +0530

    Project for mycaptain
    These two files have a project of 1)Area of the circle and 2)Identify extension

---
+++ extensionproject.py
@@ -0,0 +1,6 @@
+# Extension Project
+filename = input("Input the Filename: ")
+mylist={"py":"Python"}
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+#By Kshitij Kakde
